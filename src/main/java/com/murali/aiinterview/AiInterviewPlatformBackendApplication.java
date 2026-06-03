f873e3f32839ccaf94d1e5816b264cf3b864f268
murali-gude <gudemurali13@gmail.com>
2026-06-03 23:38:01 +0530

    Initial Spring Boot setup and first API

---
+++ src/main/java/com/murali/aiinterview/AiInterviewPlatformBackendApplication.java
@@ -0,0 +1,13 @@
+package com.murali.aiinterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiInterviewPlatformBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiInterviewPlatformBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/murali/aiinterview/AiInterviewPlatformBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.murali.aiinterview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiInterviewPlatformBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
