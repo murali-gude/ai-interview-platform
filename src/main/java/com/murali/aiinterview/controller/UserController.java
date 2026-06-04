b6ad2207b89539b5649cb22539c653f48184da93
murali-gude <gudemurali13@gmail.com>
2026-06-04 23:04:03 +0530

    Added User Entity and REST APIs

---
+++ src/main/java/com/murali/aiinterview/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.murali.aiinterview.controller;
+
+import com.murali.aiinterview.entity.User;
+import com.murali.aiinterview.repository.UserRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
